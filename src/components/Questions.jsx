2c6cf48f92c611d78028a75c1e33b44469f7b18c
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-13 16:27:19 +0000

    add initial state hooks

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react";
 
 export default function Questions({onEndGame}) {
+     const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
 
     return (
         <div> Questions </div>
