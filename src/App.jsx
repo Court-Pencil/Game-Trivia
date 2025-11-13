9c7d6aa9a25ca47e7f64164bde7d1aeca0b3039a
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-13 15:34:24 +0000

    import components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-
+import Start from './components/Start';
+import Questions from './components/Questions';
+import EndScreen from './components/EndScreen';
 import './App.css'
 import './index.css'
 
