a5ea549b2f85a08be2cc96fad9aff89de0ebd9b0
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-13 16:40:02 +0000

    add answer buttons, add handle answer section, add feeback styles to buttons

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,7 +4,8 @@ export default function Questions({onEndGame}) {
      const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
-
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [showFeedback, setShowFeedback] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -46,6 +47,16 @@ export default function Questions({onEndGame}) {
     console.log("useEffect called");
   }, []);
 
+   const handleAnswerClick = (answer) => {
+    if (selectedAnswer) return;
+    setSelectedAnswer(answer);
+    setShowFeedback(true);
+
+    if (answer === questions[currentQuestionIndex].correct_answer) {
+      setScore(score + 1);
+    }
+  };
+
     return (
          <div className="quiz-container">
       <h1>Video Game Trivia</h1>
@@ -57,6 +68,32 @@ export default function Questions({onEndGame}) {
 
       <div className="question-text">
         <h2>{questions[currentQuestionIndex].question}</h2>
+      </div>
+      <div className="answer-buttons">
+        {questions[currentQuestionIndex].all_answers.map((answer, index) => {
+          const isCorrect =
+            answer === questions[currentQuestionIndex].correct_answer;
+          const isSelected = answer === selectedAnswer;
+
+          let buttonClass = "answer-btn";
+          if (showFeedback) {
+            if (isCorrect) {
+              buttonClass += " correct";
+            } else if (isSelected && !isCorrect) {
+              buttonClass += " incorrect";
+            }
+          }
+          return (
+            <button
+              key={index}
+              onClick={() => handleAnswerClick(answer)}
+              className={buttonClass}
+              disabled={showFeedback}
+            >
+              {answer}
+            </button>
+          );
+        })}
       </div>
       </div>
     )
