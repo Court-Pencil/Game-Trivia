53bba6077048543a197118ce60f40a9fd3b22cb3
courtneyhulse97@outlook.com <Courtneyhulse97@outlook.com>
2025-11-13 16:31:47 +0000

    add fetch useEffect to add API and fetch questions

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -8,6 +8,44 @@ export default function Questions({onEndGame}) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const decodeHtmlEntities = (text) => {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = text;
+    return textArea.value;
+  };
+
+  useEffect(() => {const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://opentdb.com/api.php?amount=10&category=15&type=multiple`
+      );
+      const data = await response.json();
+      console.log("fetched data:", data);
+      const formattedQuestions = data.results.map((q) => ({
+        question: decodeHtmlEntities(q.question),
+        correct_answer: decodeHtmlEntities(q.correct_answer),
+        incorrect_answers: q.incorrect_answers.map((ans) =>
+          decodeHtmlEntities(ans)
+        ),
+        all_answers: [
+          ...q.incorrect_answers.map((a) => decodeHtmlEntities(a)),
+          decodeHtmlEntities(q.correct_answer),
+        ].sort(),
+      }));
+
+      setQuestions(formattedQuestions);
+      if (formattedQuestions.length > 0) {
+        setCurrentQuestionIndex(0);
+      }
+      setLoading(false);
+    } catch (e) {
+      setError("Failed to fetch questions. Please try again later.");
+    }
+  };
+    fetchData();
+    console.log("useEffect called");
+  }, []);
+
     return (
         <div> Questions </div>
     )
